822998146b9f0cad48960cc3462c6188c1ab9980
marrrkk <makuto4649@gmail.com>
2022-02-12 20:39:42 +0900

    新規登録時のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, presence: true
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
+
+  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  
 end
