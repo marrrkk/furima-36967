1ab4254938cc6e5fc5aadf0dafcd08e72fb5850b
marrrkk <makuto4649@gmail.com>
2022-02-17 19:57:42 +0900

    コントローラー、バリデーションの作成など

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one    :purchase
+
+  validates :product_name, :product_description, :category_id, :product_condition_id, :shipping_charges_id, :shipping_area_id, :shipping_days_id, :price, :user, presence: true
 end
