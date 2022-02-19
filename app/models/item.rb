5733944db691860488aa142de5ab611d0063bd5d
marrrkk <makuto4649@gmail.com>
2022-02-19 22:55:14 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,14 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :product_name, :product_description, :category_id, :product_condition_id, :shipping_charges_id, :shipping_area_id, :shipping_days_id, :price, :user, presence: true
+
+  validates :category_id, :product_condition_id, :shipping_charges_id, :shipping_area_id, :shipping_days_id, numericality: { other_than: 1, message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :shipping_charges
+  belongs_to :shipping_area
+  belongs_to :shipping_days
 end
+
