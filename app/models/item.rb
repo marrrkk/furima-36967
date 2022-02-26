eeae4723a2fa20e77167f47b7795b75aff495713
marrrkk <makuto4649@gmail.com>
2022-02-26 21:51:54 +0900

    コードレビュー後再編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one    :purchase
+  # has_one    :purchase
   has_one_attached :image
 
   validates :product_name, :product_description, :category_id, :product_condition_id, :shipping_charges_id, :shipping_area_id,
